27867612ddd907cdd5933b4965f6eb8b6ee921f9
Seth Mwebi <sethmwebi27@gmail.com>
2023-05-04 13:44:37 +0300

    Tweet Component

--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -1,47 +1,92 @@
-import { View, Text, Image, StyleSheet } from 'react-native'
-import { TweetType } from '../types';
+import { View, Text, Image, StyleSheet } from "react-native";
+import { Entypo, EvilIcons } from "@expo/vector-icons";
+import { TweetType } from "../types";
 
+type IconButtonProps = {
+	icon: React.ComponentProps<typeof EvilIcons>["name"];
+	text?: number | string;
+}
 
 type TweetProps = {
 	tweet: TweetType;
-}
+};
+
+const IconButton = ({ icon, text}: IconButtonProps) => {
+	return (
+		<View style={{ flexDirection: "row", alignItems: "center" }}>
+			<EvilIcons name={icon} size={22} color="gray" />
+			<Text style={{ fontSize: 12, color: "gray" }}>{text}</Text>
+		</View>
+	);
+};
 
 const Tweet = ({ tweet }: TweetProps) => {
 	return (
 		<View style={styles.container}>
-      <Image src={tweet.user.image} style={styles.userImage} />
-      <View style={styles.mainContainer}>
-        <Text style={styles.name}>{tweet.user.name}</Text>
-        <Text style={styles.content}>{tweet.content}</Text>
-      </View>
-    </View>
-	)
-}
+			<Image src={tweet.user.image} style={styles.userImage} />
+			<View style={styles.mainContainer}>
+				<View style={{ flexDirection: "row" }}>
+					<Text style={styles.name}>{tweet.user.name}</Text>
+					<Text style={styles.username}>{tweet.user.username} &bull; 2h</Text>
+					<Entypo
+						name="dots-three-horizontal"
+						size={16}
+						color="gray"
+						style={{ marginLeft: "auto" }}
+					/>
+				</View>
+				<Text style={styles.content}>{tweet.content}</Text>
+				{tweet.image && <Image src={tweet.image} style={styles.image} />}
+				<View style={styles.footer}>
+					<IconButton icon="comment" text={tweet.numberOfComments} />
+					<IconButton icon="retweet" text={tweet.numberOfRetweets}/>
+					<IconButton icon="chart" text={tweet.impressions || 0}/>
+					<IconButton icon="share-google" />
+				</View>
+			</View>
+		</View>
+	);
+};
 
-export default Tweet
+export default Tweet;
 
 const styles = StyleSheet.create({
-  container: {
-    flexDirection: "row",
-    padding: 10,
-    borderBottomWidth: StyleSheet.hairlineWidth,
-    borderBottomColor: 'lightgrey',
-    backgroundColor: "white"
-  },
-  userImage: {
-    width: 50,
-    height: 50,
-    borderRadius: 50,
-  },
-  mainContainer: {
-    marginLeft: 10,
-    flex: 1
-  },
-  name: {
-    fontWeight: "500"
-  },
-  content: {
-    lineHeight: 20,
-    marginTop: 5 
-  }
+	container: {
+		flexDirection: "row",
+		padding: 10,
+		borderBottomWidth: StyleSheet.hairlineWidth,
+		borderBottomColor: "lightgrey",
+		backgroundColor: "white",
+	},
+	userImage: {
+		width: 50,
+		height: 50,
+		borderRadius: 50,
+	},
+	mainContainer: {
+		marginLeft: 10,
+		flex: 1,
+	},
+	name: {
+		fontWeight: "500",
+	},
+	username: {
+		color: "gray",
+		marginLeft: 5,
+	},
+	content: {
+		lineHeight: 20,
+		marginTop: 5,
+	},
+	image: {
+		width: "100%",
+		aspectRatio: 16 / 9,
+		marginVertical: 10,
+		borderRadius: 15,
+	},
+	footer: {
+		flexDirection: "row",
+		marginVertical: 5,
+		justifyContent: "space-between"
+	},
 });

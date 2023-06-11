68264dfb4b2ffe0487ba13d2fb3e0d51a44b5ad5
Seth Mwebi <sethmwebi27@gmail.com>
2023-06-11 15:50:59 +0300

    prisma schema & CRUD operations

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,33 +1,56 @@
 import { Router } from "express";
+import { PrismaClient } from "@prisma/client";
 
-const router = Router()
+const router = Router();
+const prisma = new PrismaClient();
 
 // Create user
-router.post("/", (req, res) => {
-	return res.status(501).json({error: "not implemented"})
-})
+router.post("/", async (req, res) => {
+	const { email, name, username } = req.body;
+	try {
+		const result = await prisma.user.create({
+			data: { email, name, username, bio: "Hello, I'm new on twitter" },
+		});
+		return res.json(result);
+	} catch (e) {
+		return res.json({ error: "Username and email should be unique" });
+	}
+});
 
 // List user
-router.get("/", (req, res) => {
-	return res.status(501).json({error: "not implemented"})
-})
+router.get("/", async (req, res) => {
+	const allUsers = await prisma.user.findMany();
+	return res.json(allUsers);
+});
 
 // Get one user
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
 	const { id } = req.params;
-	return res.status(501).json({error: `not implemented: ${id}`})
-})
+	const user = await prisma.user.findUnique({ where: { id: Number(id) } });
+	return res.json(user);
+});
 
 // update user
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
 	const { id } = req.params;
-	return res.status(501).json({error: `not implemented: ${id}`})
-})
+	const { bio, name, image } = req.body;
+
+	try {
+		const result = await prisma.user.update({
+			where: { id: Number(id) },
+			data: { bio, name, image },
+		});
+		return res.json(result);
+	} catch (err) {
+		return res.status(400).json({ error: `Failed to update the user` });
+	}
+});
 
 // delete user
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
 	const { id } = req.params;
-	return res.status(501).json({error: `not implemented: ${id}`})
-})
+	await prisma.user.delete({ where: { id: Number(id)}})
+	return res.sendStatus(200)
+});
 
 export default router;
